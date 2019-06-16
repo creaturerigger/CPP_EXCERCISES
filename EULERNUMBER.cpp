81ec66e7701c2e555b38af38b81fd35a7b57f94f
VOLKAN BAKIR <davidabrahambaker@gmail.com>
2019-06-16 21:19:16 +0300

    Update EULERNUMBER.cpp

--- EULERNUMBER.cpp
+++ EULERNUMBER.cpp
@@ -4,10 +4,11 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
+    
+    double const ACTUAL_EULER_NUMBER = 2.718281828459045; //actual with precision of 1e-15
 
-    double const ACTUAL_EULER_NUMBER = 2.718281828459045;
-
-    cout << "Enter number of terms to calculate approximate value of euler number (-1 to quit): ";
+    // User is asked to enter number of terms for a taylor polynomial
+    cout << "Enter number of terms for a taylor polynomial to \ncalculate approximate value of euler number (-1 to quit): "; 
     int n = 0;
     cin >> n;
     while(n!=-1){
@@ -20,7 +21,7 @@ int main(int argc, char* argv[]) {
             int denomresult = 1;
             double eulernumber = 1.0;
             while(n!=0){
-                // setting denominator for each iteration with a while loop
+                // setting denominator for each iteration with a while loop (factorial)
                 while(denomtemp!=0){
                     denomresult *= denomtemp;
                     denomtemp--;
@@ -34,9 +35,10 @@ int main(int argc, char* argv[]) {
             cout << setprecision(15) << fixed;
             cout << "Approximate value of euler number is : " << eulernumber << endl;
             double relativeError = (abs(ACTUAL_EULER_NUMBER-eulernumber)/ACTUAL_EULER_NUMBER)*100;
-            cout << "Relative error of the result in percentage is : " << relativeError << endl;
-            cout << "Relative error of the result in real is : " << relativeError/100 << endl;
+            cout << "Relative error of the result in percentage is : " << relativeError << endl; // percent value of relative error
+            cout << "Relative error of the result in real is : " << relativeError/100 << endl; // actual value of relative error
         }
+        // It iteratively asks user to enter number of terms for taylor polynomial to calculate approximate euler number 
         cout << "Enter number of terms to calculate approximate value of euler number (-1 to quit): ";
         cin >> n;
     }
