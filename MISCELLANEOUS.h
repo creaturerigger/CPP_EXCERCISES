dc198d800e733b2fa97f01eb714ed30c9a1d0c45
VOLKAN BAKIR <volkan_bakir@hotmail.com>
2019-06-16 23:18:05 +0300

    16-06-2019 23:17

---
+++ MISCELLANEOUS.h
@@ -0,0 +1,19 @@
+//
+// Created by volka on 16/06/2019.
+//
+
+#ifndef CPP_EXCERCISES_MISCELLANEOUS_H
+#define CPP_EXCERCISES_MISCELLANEOUS_H
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+class MISCELLANEOUS {
+public:
+    void printIntegers() const;
+};
+
+
+#endif //CPP_EXCERCISES_MISCELLANEOUS_H
